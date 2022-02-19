16b112eb06008ba6b422e0989ae957b8e5f283dd
Alena Karenina <karenina@corp.profi.travel>
2022-02-19 23:27:17 +0500

    add function length string

--- js/main.js
+++ js/main.js
@@ -1,19 +1,21 @@
-function getRandomInt(min, max) {
-
-  if (min < 0 || max < 0) {
-    alert('Ошибка! Число не может быть меньше нуля.');
-    return;
-  }
-
-  if (max <= min) {
-    alert('Ошибка! Наибольшее число не может быть меньше или равно наименьшему числу.');
-    return;
-  }
-
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min)) + min; //Максимум не включается, минимум включается
-}
-
-getRandomInt (0, 10);
 // Функция с сайта MDN: https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+const getRandomInt = function(min, max) {
+  if(min >= 0 && max >= 0) {
+    return Math.random() * (max - min) + min;
+  }
+
+  return 'число меньше нуля';
+};
+
+const getLengthString = function(string, maxLength) {
+  if(string.length > maxLength) {
+    return true;
+  }
+
+  else {
+    return false;
+  }
+};
+
+getRandomInt (1, 10);
+getLengthString('Строка, не более 160 символов', 160);
