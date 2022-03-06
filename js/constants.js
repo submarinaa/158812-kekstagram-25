99d8f423f034e92154cbf24ee3bab6016ac3909c
Alena Karenina <karenina@corp.profi.travel>
2022-03-06 14:39:08 +0500

    разбила на модули

---
+++ js/constants.js
@@ -0,0 +1,23 @@
+const MESSAGES_COMMENTS = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
+];
+const NAMES_USERS = [
+  'Артём',
+  'Петя',
+  'Ольга',
+  'Светлана'
+];
+const MAX_ITEM_ID = 25;
+const IMG_FOLDER = 'photos/';
+const IMG_EXTEND = '.jpg';
+const MIN_LIKES = 15;
+const MAX_LIKES = 200;
+const AVATAR_FOLDER = 'img/avatar-';
+const AVATAR_EXTEND = '.svg';
+
+export {MESSAGES_COMMENTS, NAMES_USERS, MAX_ITEM_ID, IMG_FOLDER, IMG_EXTEND, MIN_LIKES, MAX_LIKES, AVATAR_FOLDER, AVATAR_EXTEND};
---
+++ js/utils.js
@@ -0,0 +1,18 @@
+const getRandomInt = function (min = 0, max = 0) {
+  return Math.floor(Math.random() * (max - min) + min);
+};
+
+function shuffle(array) {
+  array.sort(() => Math.random() - 0.5);
+}
+
+const getSuffleIntArray = function (length) {
+  const arr = [];
+  for (let integer = 0; integer < length; integer++) {
+    arr.push(integer);
+  }
+  shuffle(arr);
+  return arr;
+};
+
+export {getRandomInt, getSuffleIntArray};
