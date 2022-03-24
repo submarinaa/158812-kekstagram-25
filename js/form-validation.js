bfef94fa61435fa42b7b257371415a3c2f27b3ff
Alena Karenina <karenina@corp.profi.travel>
2022-03-24 19:15:51 +0500

    fix styles colors

--- js/form-validation.js
+++ js/form-validation.js
@@ -21,10 +21,10 @@ const pristine = new Pristine(formUploadImage, {
 //Максимальная длина комментария - не более 140
 const validateDescription = (str) => {
   if(str.length >= 1 && str.length <= MAX_DESCRIPTION - 1) {
-    textarea.style.outline = '';
+    textarea.style.outlineColor = 'transparent';
     return true;
   }
-  textarea.style.outline = '2px solid red';
+  textarea.style.outlineColor = 'red';
 };
 pristine.addValidator(textarea, validateDescription, `Комментарий максимум ${MAX_DESCRIPTION} символов`);
 
@@ -44,8 +44,10 @@ const getHashtags = (str) => {
 //Количество хэш-тегов - не более 5
 const checkCountHashtags = (tags) => {
   if(tags.length === 0) {
+    hashtagInput.style.outlineColor = 'transparent';
     return true;
   }
+  hashtagInput.style.outlineColor = 'red';
   return getHashtags(tags).length <= MAX_HASHTAGS;
 };
 pristine.addValidator(hashtagInput, checkCountHashtags, `Максимум ${MAX_HASHTAGS} хэш-тегов`);
@@ -53,9 +55,10 @@ pristine.addValidator(hashtagInput, checkCountHashtags, `Максимум ${MAX_
 //строка после решётки должна состоять из букв и чисел и не может содержать пробелы, спецсимволы (#, @, $ и т. п.), символы пунктуации (тире, дефис, запятая и т. п.), эмодзи и т. д.
 const validateString = (str) => {
   if (str.length === 0) {
-    hashtagInput.style.outline = '';
+    hashtagInput.style.outlineColor = 'transparent';
     return true;
   }
+  hashtagInput.style.outlineColor = 'red';
   const hashtags = getHashtags(str);
   const results = [];
   hashtags.forEach((element) => {
@@ -63,12 +66,13 @@ const validateString = (str) => {
   });
   return results.indexOf(false) === -1;
 };
-pristine.addValidator(hashtagInput, validateString, 'Хэш-тег должен состоять только из букв и чисел, должен начинаться с #');
+pristine.addValidator(hashtagInput, validateString, 'Хэш-тег не должен содержать спецсимволов и т.д., должен начинаться с решетки #');
 
 //Отсутствие одинаковых хэш-тегов
 const checkHashtagsDuplicate = (value) => {
   const splitStrings = (str) => str.trim().toLowerCase().split(' ').filter((tag) => (tag !== ''));
   const hashtags = splitStrings(value);
+  hashtagInput.style.outlineColor = 'red';
   return !(hashtags.some((tag, index) => hashtags.indexOf(tag) !== index));
 };
 pristine.addValidator(hashtagInput, checkHashtagsDuplicate, 'Такой хэш-тег уже существует');
@@ -78,6 +82,7 @@ const onUploadForm = (evt) => {
   if (!isValid) {
     evt.preventDefault();
     pristine.reset();
+    return false;
   }
   formUploadImage.submit();
 };
