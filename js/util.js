a13c1eff643c101c5ca83bd1c66ae6f60f84623c
Alena Karenina <karenina@corp.profi.travel>
2022-04-05 22:20:03 +0500

    убрала неиспользуемое, переименовала обработчики

--- js/util.js
+++ js/util.js
@@ -1,20 +1,3 @@
-const getRandomInt = function (min = 0, max = 0) {
-  return Math.floor(Math.random() * (max - min) + min);
-};
-
-function shuffle(array) {
-  array.sort(() => Math.random() - 0.5);
-}
-
-const getSuffleIntArray = function (length) {
-  const arr = [];
-  for (let integer = 0; integer < length; integer++) {
-    arr.push(integer);
-  }
-  shuffle(arr);
-  return arr;
-};
-
 const isEscapePressed = (evt) => evt.key === 'Escape';
 
-export {getRandomInt, getSuffleIntArray, isEscapePressed};
+export {isEscapePressed};
