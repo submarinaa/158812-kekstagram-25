4fa011fc69b03f8c4787977213556501199c1150
Alena Karenina <karenina@corp.profi.travel>
2022-04-07 17:53:44 +0500

    добавление фильтрации с устранением дребезга

---
+++ js/filter.js
@@ -0,0 +1,54 @@
+//Модуль, отвечающий за фильтрацию изображений
+import {QUANTITY_RANDOM_PICTURES, TIMEOUT_DELAY} from './constants.js';
+import {generateUniqueElements, debounce} from './util.js';
+import {createPicturesFragment} from './pictures.js';
+
+const filters = document.querySelector('.img-filters');
+const filtersButton = filters.querySelectorAll('.img-filters__button');
+
+const filterRandom = (pictures) => Array.from(generateUniqueElements(pictures, QUANTITY_RANDOM_PICTURES));
+
+const filterDefault = (pictures) => Array.from(pictures.slice());
+
+const filterDiscussed = (pictures) => Array.from(pictures.slice().sort((commentA, commentB) => commentB.comments.length - commentA.comments.length));
+
+const removeFilterButtonActive = () => {
+  document.querySelector('#filter-discussed').classList.remove('img-filters__button--active');
+  document.querySelector('#filter-random').classList.remove('img-filters__button--active');
+  document.querySelector('#filter-default').classList.remove('img-filters__button--active');
+};
+
+const clearPictures = () => {
+  const getPicturesContainer = () => document.querySelectorAll('.picture');
+  getPicturesContainer().forEach((picture) => picture.remove());
+};
+
+const renderPicturesFilter = (pictures) => {
+  clearPictures();
+  createPicturesFragment(pictures);
+};
+
+const filterPictures = (pictures) => {
+  filtersButton.forEach((btn) => {
+    filters.classList.remove('img-filters--inactive');
+
+    btn.addEventListener('click',
+      debounce((evt) => {
+        removeFilterButtonActive();
+
+        evt.target.classList.add('img-filters__button--active');
+
+        if (evt.target.matches('#filter-default')) {
+          renderPicturesFilter(filterDefault(pictures));
+        } else if (evt.target.matches('#filter-random')) {
+          renderPicturesFilter(filterRandom(pictures));
+        } else if (evt.target.matches('#filter-discussed')) {
+          renderPicturesFilter(filterDiscussed(pictures));
+        }
+      },
+      TIMEOUT_DELAY),
+    );
+  });
+};
+
+export {filterPictures};
--- js/util.js
+++ js/util.js
@@ -1,3 +1,47 @@
+//Модуль со вспомогательными функциями
+function getRandomPositiveInteger (a, b) {
+  const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
+  const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
+}
+
+function generateUniqueElements (elements, quantity) {
+  const arr = [];
+  let count = 0;
+  while (count < quantity) {
+    let randomElement = elements[getRandomPositiveInteger(1, elements.length - 1)];
+
+    while (arr.filter((element) => element.id === randomElement.id).length > 0) {
+      randomElement = elements[getRandomPositiveInteger(1, elements.length - 1)];
+    }
+    arr.push(randomElement);
+    count++;
+  }
+  return arr;
+}
+
+function debounce (callback, timeoutDelay = 500) {
+  let timeoutId;
+
+  return (...rest) => {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => callback.apply(this, rest), timeoutDelay);
+  };
+}
+
+function throttle (callback, delayBetweenFrames) {
+  let lastTime = 0;
+
+  return (...rest) => {
+    const now = new Date();
+    if (now - lastTime >= delayBetweenFrames) {
+      callback.apply(this, rest);
+      lastTime = now;
+    }
+  };
+}
+
 const isEscapePressed = (evt) => evt.key === 'Escape';
 
-export {isEscapePressed};
+export {isEscapePressed, generateUniqueElements, debounce, throttle};
